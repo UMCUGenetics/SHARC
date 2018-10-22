c3e472ddc9730c4d8a9697945a83691709676a20
Mark van Roosmalen <mroosmalen@cog146.op.umcutrecht.nl>
2018-10-22 15:22:01 +0200

    Make vcf_to_fasta.py suitable for other vcf files

--- scripts/vcf_to_fasta.py
+++ scripts/vcf_to_fasta.py
@@ -1,6 +1,7 @@
 import json, requests
 import vcf as pyvcf
 import argparse
+import sys
 
 server = "https://grch37.rest.ensembl.org"
 
@@ -32,12 +33,25 @@ def mask_seq( chr, start, end, strand, seq ):
 
 
 def get_seq(chr, start, end, strand):
-    ext = "/sequence/region/human/"+str(chr)+":"+str(start)+"-"+str(end)+":"+str(strand)+""
+    ext = "/info/assembly/homo_sapiens/"+str(chr)
     request = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+    #sys.stderr.write(str(request.text)+"\n")
     data = json.loads(request.text)
-    seq = data['seq']
-    if mask:
-        seq = mask_seq(chr, start, end, strand, seq )
+    chrlength = data['length']
+
+    ext = "/sequence/region/human/"+str(chr)+":"+str(start)+"-"+str(end)+":"+str(strand)+""
+    if start < 1:
+        sys.stderr.write("\tFailed to get seq, because POS is too close to START of the chr\n")
+        seq = False
+    elif end > chrlength:
+        sys.stderr.write("\tFailed to get seq, because ENDPOS is too close to END of the chr\n")
+        seq = False
+    else:
+        request = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+        data = json.loads(request.text)
+        seq = data['seq']
+        if mask:
+            seq = mask_seq(chr, start, end, strand, seq )
     return( seq )
 
 # def reverse_complement( seq ):
@@ -50,6 +64,9 @@ def get_seq(chr, start, end, strand):
 def alt_convert( record ):
     orientation = None
     remoteOrientation = None
+    if record.INFO['SVTYPE'] == 'INS':
+        orientation = False
+        remoteOrientation = True
     if record.INFO['SVTYPE'] == 'DEL':
         orientation = False
         remoteOrientation = True
@@ -62,6 +79,19 @@ def alt_convert( record ):
     elif 'INV5' in record.INFO:
         orientation = True
         remoteOrientation = True
+    elif 'STRANDS' in record.INFO:
+        if record.INFO['STRANDS'][0] == '++':
+            orientation = False
+            remoteOrientation = False
+        elif record.INFO['STRANDS'][0] == '--':
+            orientation = True
+            remoteOrientation = True
+        elif record.INFO['STRANDS'][0] == '-+':
+            orientation = True
+            remoteOrientation = False
+        elif record.INFO['STRANDS'][0] == '+-':
+            orientation = False
+            remoteOrientation = True
 
     if 'CT' in record.INFO:
         if record.INFO['CT'] == '3to5':
@@ -77,25 +107,35 @@ def alt_convert( record ):
             orientation = True
             remoteOrientation = True
     if orientation is None or remoteOrientation is None:
-        sys.exit()
-    record.ALT = [ pyvcf.model._Breakend( record.CHROM, record.INFO['END'], orientation, remoteOrientation, record.REF, True ) ]
+        sys.stderr.write( "\tFailed to convert orientation"+"\n" )
+        record = False
+        # sys.exit()
+    else:
+        record.ALT = [ pyvcf.model._Breakend( record.CHROM, record.INFO['END'], orientation, remoteOrientation, record.REF, True ) ]
     return( record )
 
 vcf_reader = pyvcf.Reader(open(vcf, 'r'))
 for record in vcf_reader:
-    if ( isinstance(record.ALT[0], pyvcf.model._SV) and not "INS" in str(record.ALT[0]) ) or ( isinstance( record.ALT[0], pyvcf.model._Substitution ) and len(record.ALT[0]) < len(record.REF) ):
+    # if ( isinstance(record.ALT[0], pyvcf.model._SV) and not "INS" in str(record.ALT[0]) ) or ( isinstance( record.ALT[0], pyvcf.model._Substitution ) and len(record.ALT[0]) < len(record.REF) ):
+    sys.stderr.write( str(record) )
+    if isinstance(record.ALT[0], pyvcf.model._SV) or isinstance( record.ALT[0], pyvcf.model._Substitution ):
         record = alt_convert( record )
+    if not record:
+        continue
     seq1 = ''
     seq2 = ''
     if record.ALT[0].orientation:
         seq1 = get_seq(record.CHROM, record.POS+offset, record.POS+offset+flank, -1)
     else:
         seq1 = get_seq(record.CHROM, record.POS-flank-offset, record.POS-offset, 1)
-
+    if not seq1:
+        continue
     if record.ALT[0].remoteOrientation:
         seq2 = get_seq(record.ALT[0].chr, record.ALT[0].pos+offset, record.ALT[0].pos+offset+flank, 1)
     else:
         seq2 = get_seq(record.ALT[0].chr, record.ALT[0].pos-offset-flank, record.ALT[0].pos-offset, -1)
-
+    if not seq2:
+        continue
+    sys.stderr.write("\tSucces\n")
     print(">"+record.ID)
     print( seq1+"[]"+seq2 )
