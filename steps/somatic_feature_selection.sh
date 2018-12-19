a8f7e54afed38a37d4dd98a6b88d00f4c288e265
SdeBlank <Sam@de-blank.nl>
2018-12-19 12:32:36 +0100

    New somatic feature selection

--- steps/somatic_feature_selection.sh
+++ steps/somatic_feature_selection.sh
@@ -5,12 +5,15 @@ echo "
 Required parameters:
     -v|--vcf		    Path to vcf file
     -c|--cancer_type  Cancer type examined
+    -a|--attributes_output  Output for feature table
 
 Optional parameters:
     -h|--help       Shows help
     -s|--script     Path to vcf_primer_filter.py [$SCRIPT]
     -o|--output     VCF output file [$OUTPUT]
     -f|--flank      Added region around BND to compare to genes [$FLANK]
+    -id|--icgc_database_directory   Directory containing ICGC files for genes in cancer types[$ICGC_DATABASE_DIRECTORY]
+    -cb|--cosmic_breakpoints  COSMIC .csv containing known breakpoints for a specific cancer type [$COSMIC_BREAKPOINTS]
     -p|--support    Minimal fraction of patients needed to annotate as cancer gene [$SUPPORT]
     -e|--venv       Path to virtual environment[$VENV]
 "
@@ -19,10 +22,12 @@ Optional parameters:
 POSITIONAL=()
 
 #DEFAULT
-SCRIPT="/hpc/cog_bioinf/kloosterman/common_scripts/sharc/scripts/gene_annotation_ICGC.py"
+SCRIPT="/hpc/cog_bioinf/kloosterman/common_scripts/sharc/scripts/somatic_feature_selection.py"
 OUTPUT="/dev/stdout"
+ICGC_DATABASE_DIRECTORY="/hpc/cog_bioinf/kloosterman/common_scripts/sharc/files/"
+COSMIC_BREAKPOINTS=None
+SUPPORT=None
 FLANK=200
-SUPPORT=0.05
 VENV='/hpc/cog_bioinf/kloosterman/common_scripts/sharc/venv/bin/activate'
 
 while [[ $# -gt 0 ]]
@@ -44,6 +49,11 @@ do
     shift # past argument
     shift # past value
     ;;
+    -a|--attributes_output)
+    ATTRIBUTES_OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
     -s|--script)
     SCRIPT="$2"
     shift # past argument
@@ -56,6 +66,16 @@ do
     ;;
     -p|--support)
     SUPPORT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -id|--icgc_database_directory)
+    ICGC_DATABASE_DIRECTORY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -cb|--cosmic_breakpoints)
+    COSMIC_BREAKPOINTS="$2"
     shift # past argument
     shift # past value
     ;;
@@ -85,12 +105,24 @@ elif [ -z $TYPE ]; then
     echo "Missing -c|--cancer_type parameter"
     usage
     exit
+
+elif [ -z $ATTRIBUTES_OUTPUT ]; then
+    echo "Missing -a|--attributes_output"
+    usage
+    exit
 fi
 
 echo `date`: Running on `uname -n`
 
 . $VENV
 
-python $SCRIPT -c $TYPE -f $FLANK -s $SUPPORT -o $OUTPUT $VCF
+if [ $SUPPORT == None ];then
+  python $SCRIPT -c $TYPE -a $ATTRIBUTES_OUTPUT -f $FLANK -id $ICGC_DATABASE_DIRECTORY -cb $COSMIC_BREAKPOINTS -o $OUTPUT $VCF
+elif [ $COSMIC_BREAKPOINTS == None ];then
+  python $SCRIPT -c $TYPE -a $ATTRIBUTES_OUTPUT -f $FLANK -s $SUPPORT -id $ICGC_DATABASE_DIRECTORY -o $OUTPUT $VCF
+elif [ $SUPPORT == None ] && [ $COSMIC_BREAKPOINTS == None ];then
+  python $SCRIPT -c $TYPE -a $ATTRIBUTES_OUTPUT -f $FLANK -id $ICGC_DATABASE_DIRECTORY -o $OUTPUT $VCF
+else
+  python $SCRIPT -c $TYPE -a $ATTRIBUTES_OUTPUT -f $FLANK -s $SUPPORT -id $ICGC_DATABASE_DIRECTORY -cb $COSMIC_BREAKPOINTS -o $OUTPUT $VCF
 
 echo `date`: Done
